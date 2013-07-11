e35ce2c7e52792b1b5fd6f455d6b679370be7386
Peter Lejeck <peter.lejeck@gmail.com>
2013-07-10 21:59:09 -0700

    Add support for plugins (I think)
    Really, I haven't tested any of this code, I've only prayed for success.

--- init.zsh
+++ init.zsh
@@ -5,3 +5,13 @@ fpath=(${PWD}/functions $PWD/completions $fpath)
 for config file ($PWD/lib/*.zsh); do
 	source $config_file
 done
+
+for plugin ($omz_plugins); do
+	if [[ -f "$PWD/plugins/$plugin/$plugin.plugin.zsh"
+	 || -f "$PWD/plugins/$plugin/$plugin/_$plugin" ]]; then
+		fpath=($PWD/plugins/$plugin $fpath)
+	fi
+	if [[ -f "$PWD/plugins/$plugin/$plugin.plugin.zsh" ]]; then
+		source $PWD/plugins/$plugin/$plugin.plugin.zsh
+	fi
+done
