e71fd3582967d1e45dfd3ed5b33872cf12b28bcf
Peter Lejeck <peter.lejeck@gmail.com>
2013-07-10 22:04:55 -0700

    Fix mistake in directories; the shit's in ./oh-my-zsh

--- init.zsh
+++ init.zsh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
-fpath=($PWD/functions $PWD/completions $fpath)
+fpath=($PWD/oh-my-zsh/functions $PWD/oh-my-zsh/completions $fpath)
 
-for config file ($PWD/lib/*.zsh); do
+for config_file ($PWD/oh-my-zsh/lib/*.zsh); do
 	source $config_file
 done
 
 for plugin ($omz_plugins); do
-	if [[ -f "$PWD/plugins/$plugin/$plugin.plugin.zsh"
-	 || -f "$PWD/plugins/$plugin/$plugin/_$plugin" ]]; then
-		fpath=($PWD/plugins/$plugin $fpath)
+	if [[ -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh"
+	 || -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin/_$plugin" ]]; then
+		fpath=($PWD/oh-my-zsh/plugins/$plugin $fpath)
 	fi
-	if [[ -f "$PWD/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-		source $PWD/plugins/$plugin/$plugin.plugin.zsh
+	if [[ -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh" ]]; then
+		source $PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh
 	fi
 done
 
-for theme_file ($PWD/themes/*.zsh-theme); do
+for theme_file ($PWD/oh-my-zsh/themes/*.zsh-theme); do
 	zsh_themes[$theme_name]=${theme_file:a}
 done
