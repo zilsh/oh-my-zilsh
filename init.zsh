7108fe7b7ca8bfbb7540532e551f39cd7218ab15
George Jiglau <george@mux.ro>
2013-09-01 18:51:25 +0300

    Don't depend on the current working dir

--- init.zsh
+++ init.zsh
@@ -1,24 +1,25 @@
 #!/usr/bin/env zsh
+local OMZ_PATH="$(cd "$(dirname "$0")" && pwd)/oh-my-zsh"
 
-fpath=($PWD/oh-my-zsh/functions $PWD/oh-my-zsh/completions $fpath)
+fpath=($OMZ_PATH/functions $OMZ_PATH/completions $fpath)
 
-for config_file ($PWD/oh-my-zsh/lib/*.zsh); do
+for config_file ($OMZ_PATH/lib/*.zsh); do
 	_zilsh_debug "    Loading $config_file."
 	source $config_file
 done
 
 for plugin ($omz_plugins); do
 	_zilsh_debug "    Loading $plugin plugin."
-	if [[ -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh"
-	 || -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin/_$plugin" ]]; then
-		fpath=($PWD/oh-my-zsh/plugins/$plugin $fpath)
+	if [[ -f "$OMZ_PATH/plugins/$plugin/$plugin.plugin.zsh"
+	 || -f "$OMZ_PATH/plugins/$plugin/$plugin/_$plugin" ]]; then
+		fpath=($OMZ_PATH/plugins/$plugin $fpath)
 	fi
-	if [[ -f "$PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-		source $PWD/oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh
+	if [[ -f "$OMZ_PATH/plugins/$plugin/$plugin.plugin.zsh" ]]; then
+		source $OMZ_PATH/plugins/$plugin/$plugin.plugin.zsh
 	fi
 done
 
-for theme_file ($PWD/oh-my-zsh/themes/*.zsh-theme); do
+for theme_file ($OMZ_PATH/themes/*.zsh-theme); do
 	_zilsh_debug "    Loading ${theme_file:t} theme."
 	zsh_themes[${theme_file:t}]=${theme_file:a}
 done
