3a38bf7ca46e86620a900f1fc931fb190aaa415e
Peter Lejeck <peter.lejeck@gmail.com>
2014-04-16 23:53:33 -0700

    Remove underscores from zilsh functions

--- init.zsh
+++ init.zsh
@@ -3,12 +3,12 @@ local OMZ_PATH="$BUNDLE_DIR/oh-my-zsh"
 fpath=($OMZ_PATH/functions $OMZ_PATH/completions $fpath)
 
 for config_file ($OMZ_PATH/lib/*.zsh); do
-	_zilsh_debug "    Loading $config_file."
+	zilsh_debug "    Loading $config_file."
 	source $config_file
 done
 
 for plugin ($omz_plugins); do
-	_zilsh_debug "    Loading $plugin plugin."
+	zilsh_debug "    Loading $plugin plugin."
 	if [[ -f "$OMZ_PATH/plugins/$plugin/$plugin.plugin.zsh"
 	 || -f "$OMZ_PATH/plugins/$plugin/$plugin/_$plugin" ]]; then
 		fpath=($OMZ_PATH/plugins/$plugin $fpath)
@@ -19,6 +19,6 @@ for plugin ($omz_plugins); do
 done
 
 for theme_file ($OMZ_PATH/themes/*.zsh-theme); do
-	_zilsh_debug "    Loading ${theme_file:t} theme."
+	zilsh_debug "    Loading ${theme_file:t} theme."
 	zsh_themes[${theme_file:t}]=${theme_file:a}
 done
